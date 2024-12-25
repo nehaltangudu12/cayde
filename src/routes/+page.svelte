91adc944dff5b07ce81d4b8d033cb58b02c82eb6
FlamingSparrow <nehaltangudu12@gmail.com>
2024-12-24 20:20:38 -0600

    initial commit 3

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,6 @@
+<script>
+  let title = "Welcome to SvelteKit";
+</script>
 
-
-<h1>Welcome to SvelteKit</h1>
+<h1>{title}</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
